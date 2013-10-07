aa9cea07b0c6fdafaab0d483d2120abc250312d6
jmedrano <jmedrano@yahoo-inc.com>
2013-10-07 00:08:38 -0700

    Removed loading from a button click.

--- src/com/yahoo/cafes/MainActivity.java
+++ src/com/yahoo/cafes/MainActivity.java
@@ -2,12 +2,8 @@ package com.yahoo.cafes;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
-import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ListView;
 
 import com.yahoo.cafes.api.UrlsClient;
@@ -24,17 +20,7 @@ public class MainActivity extends Activity {
 		ListView lvLocations = (ListView) findViewById(R.id.lvLocations);
 		final ArrayAdapter<Location> adapter = new LocationArrayAdapter(this.getApplicationContext(), Cafe.getInstance().getLocations());
 		lvLocations.setAdapter(adapter);
-		
-		Button btnLoad = (Button)findViewById(R.id.btnLoad);
-		btnLoad.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				UrlsClient.getInstance().loadCafeFromApi(adapter);
-				Log.d("DEBUG", Cafe.getInstance().getLocations().toString());
-			}
-		});
-		
+		UrlsClient.getInstance().loadCafeFromApi(adapter);
 		
 	}
 
